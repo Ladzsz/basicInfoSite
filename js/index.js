15a3b6e8141d0f86a4318296d65041445188b111
Ladzsz <londonmason602@gail.com>
2025-10-14 15:02:25 -0400

    added netlify access

--- js/index.js
+++ js/index.js
@@ -14,7 +14,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Middleware
-app.use(cors());
+app.use(cors( {
+  origin: 'https://basicinfosite.netlify.app'
+}));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../'))); 
 
